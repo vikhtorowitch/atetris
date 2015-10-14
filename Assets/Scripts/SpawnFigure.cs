82548536c36a869fa54de4d595247eb530a1a47e
Aliex <alikasoft@gmail.com>
2015-10-14 17:25:53 +0300

    oop, mother of god

---
+++ Assets/Scripts/SpawnFigure.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFigure {
+
+	private int[,] _figure;
+	private int[,] _buffer;
+
+	public SpawnFigure(int[,] _inarr) {
+		_figure = _inarr;
+	}
+
+	public void DrawFigure(int[,] _destination, int _x, int _y) {
+		for (int iy=0; iy<=_figure.GetUpperBound(0); iy++)
+		for (int ix=0; ix<=_figure.GetUpperBound(1); ix++) {
+			if (_figure[iy, ix] == 1) _destination[ix+_x, iy+_y] = 1;
+		}
+	}
+
+	public void RotateFigure() {
+
+		_buffer = new int[_figure.GetUpperBound(1)+1, _figure.GetUpperBound(0)+1];
+		for (int iy=0; iy<=_figure.GetUpperBound(0); iy++)
+			for (int ix=0; ix<=_figure.GetUpperBound(1); ix++) {
+			_buffer[ix, iy] = _figure[iy, ix];
+			}
+		_figure = _buffer;
+		Debug.Log(_figure.Rank.ToString());
+		Debug.Log(_figure.GetUpperBound(0).ToString());
+		Debug.Log(_figure.GetUpperBound(1).ToString());
+		Debug.Log(_buffer.Rank.ToString());
+		Debug.Log(_buffer.GetUpperBound(0).ToString());
+		Debug.Log(_buffer.GetUpperBound(1).ToString());
+
+	}
+}
--- Assets/Scripts/SpawnGrid.cs
+++ Assets/Scripts/SpawnGrid.cs
@@ -14,27 +14,24 @@ public class SpawnGrid : MonoBehaviour {
 
 	private int[,] _arr;
 
-	private int[,] _figL = {
-		{1,0},
-		{1,0},
-		{1,0},
-		{1,1}
-	};
+	SpawnFigure figL;
 
-	
-	// Use this for initialization
 	void Start () {
 		_arr = new int[10,20];
 
 		CleArr ();
 		_arr [0, 0] = 1;
 
+		figL = new SpawnFigure(new int[,] {
+			{1,0},
+			{1,0},
+			{1,1}
+		});
 
-		Debug.Log(_figL.Rank.ToString());
-		Debug.Log(_figL.GetUpperBound(0).ToString());
-		Debug.Log(_figL.GetUpperBound(1).ToString());
+		//Debug.Log(_figL.Rank.ToString());
+		//Debug.Log(_figL.GetUpperBound(0).ToString());
+		//Debug.Log(_figL.GetUpperBound(1).ToString());
 
-			//canv = GetComponent<Canvas> ();
 		/*
 		Vector3 vec;
 		for (int iy=1; iy<11; iy++)
@@ -54,10 +51,6 @@ public class SpawnGrid : MonoBehaviour {
 	void OnGUI () {
 		for (int iy=1; iy<21; iy++)
 		for (int ix=1; ix<11; ix++) {
-			//vec.x = 
-			//Instantiate (grid_element);
-
-
 
 			if(_arr[ix-1, iy-1] == 0) GUI.Label(new Rect(ix*25, iy*25, 30, 30), texWhite);
 				else GUI.Label(new Rect(ix*25, iy*25, 30, 30), texBlue);
@@ -66,19 +59,21 @@ public class SpawnGrid : MonoBehaviour {
 
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.UpArrow))	_y--;
+		if (Input.GetKeyDown (KeyCode.UpArrow))	figL.RotateFigure ();	//_y--;
 		if (Input.GetKeyDown (KeyCode.DownArrow))	_y++;
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) _x--;
 		if (Input.GetKeyDown (KeyCode.RightArrow))	_x++;
 		CleArr ();
 		//_arr [_x, _y] = 1;
 
+		figL.DrawFigure(_arr, _x, _y);
 
+		/*
 		for (int iy=0; iy<=_figL.GetUpperBound(0); iy++)
 			for (int ix=0; ix<=_figL.GetUpperBound(1); ix++) {
 				if (_figL[iy, ix] == 1) _arr[ix+_x, iy+_y] = 1;
 			}
+			*/
 	}
 }
