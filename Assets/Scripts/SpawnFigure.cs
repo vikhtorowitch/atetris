af635490c33ee50c3dbb47f268554b4bfeedca47
Aliex <alikasoft@gmail.com>
2015-10-15 12:02:47 +0300

    falling figures, fixed figures

--- Assets/Scripts/SpawnFigure.cs
+++ Assets/Scripts/SpawnFigure.cs
@@ -9,29 +9,109 @@ public class SpawnFigure {
 	public int x = 4;
 	public int y = 0;
 
-	public SpawnFigure(int[,] _inarr) {
-		_figure = _inarr;
+	public bool flagCollision = false;
+
+	private int prev_x = 4;
+	private int prev_y = 0;
+
+	public SpawnFigure(int figureType) {
+		switch (figureType) {
+		case 0:
+			_figure = new int[,] {
+				{1,0},
+				{1,0},
+				{1,0},
+				{1,0}
+				};
+			break;
+		case 1:
+			_figure = new int[,] {
+				{1,1},
+				{1,1}
+			};
+			break;
+		case 2:
+			_figure = new int[,] {
+				{1,0},
+				{1,0},
+				{1,1}
+			};
+			break;
+		case 3:
+			_figure = new int[,] {
+				{0,1},
+				{0,1},
+				{1,1}
+			};
+			break;
+		case 4:
+			_figure = new int[,] {
+				{1,0},
+				{1,1},
+				{0,1}
+			};
+			break;
+		case 5:
+			_figure = new int[,] {
+				{0,1},
+				{1,1},
+				{1,0}
+			};
+			break;
+		case 6:
+			_figure = new int[,] {
+				{1,1,1},
+				{0,1,0}
+			};
+			break;
+
+		}
 	}
 
-	public void DrawFigure(int[,] _destination) {
+	public void DrawFigure(int[,] _destination, int[,] _fixed) {
 
 		if ((x+_figure.GetUpperBound(0)) > (_destination.GetUpperBound(0))) x = _destination.GetUpperBound(0)-_figure.GetUpperBound(0); 
 		if (x < 0) x = 0; 
 
+		if (y  > (_fixed.GetUpperBound(1) - _figure.GetUpperBound(1))) {
+			flagCollision = true;
+			x = prev_x;
+			y = prev_y;
+
+		}
+
+		for (int iy=0; iy<=_figure.GetUpperBound(1); iy++)
+			for (int ix=0; ix<=_figure.GetUpperBound(0); ix++) {
+				if ((_figure [ix, iy] == 1) && (_fixed [ix + x, iy + y] == 1)) {
+					if (iy <= y) {
+						flagCollision = true;
+					}
+				x = prev_x;
+				y = prev_y;
+			}
+			}
+		//if (!flagCollision)	
 		for (int iy=0; iy<=_figure.GetUpperBound(1); iy++)
 		for (int ix=0; ix<=_figure.GetUpperBound(0); ix++) {
-			if (_figure[ix, iy] == 1) _destination[ix+x, iy+y] = 1;
+			if(_figure[ix, iy] == 1) _destination[ix+x, iy+y] = _figure[ix, iy];
 		}
+
+		//for (int ix=0; ix<=_figure.GetUpperBound(0); ix++) 
+			//_destination [x+ix, y + _figure.GetUpperBound (1)+1] = 2;
+		//int py = _figure.GetUpperBound(1);
+
+		prev_x = x;
+		prev_y = y;
+
 	}
 
 	public void RotateFigure() {
 
 		_buffer = new int[_figure.GetUpperBound(1)+1, _figure.GetUpperBound(0)+1];
-		int ti;
 		for (int iy=0; iy<=_figure.GetUpperBound(1); iy++)
 			for (int ix=0; ix<=_figure.GetUpperBound(0); ix++) {
 			_buffer[iy, _figure.GetUpperBound(0)-ix] = _figure[ix, iy];
-			ti = _figure.GetUpperBound(0)-ix;
+			//ti = _figure.GetUpperBound(0)-ix;
 			//Debug.Log("iy: "+iy.ToString()+"ix: "+ti.ToString());
 			}
 		_figure = _buffer;
--- Assets/Scripts/SpawnGrid.cs
+++ Assets/Scripts/SpawnGrid.cs
@@ -3,27 +3,28 @@ using System.Collections;
 
 public class SpawnGrid : MonoBehaviour {
 
-	public GameObject grid_element;
+	//public GameObject grid_element;
 	public Texture texWhite;
 	public Texture texBlue;
+	public Texture texGreen;
 
 	private Canvas canv;
 
 	private int[,] _arr;
+	private int[,] _fixed;
+
+	public float fallingSpeed = 1f;
 
 	SpawnFigure figL;
 
 	void Start () {
 		_arr = new int[10,20];
+		_fixed = new int[10,20];
 
-		CleArr ();
-		_arr [0, 0] = 1;
+		CleArr (_arr);
+		CleArr (_fixed);
 
-		figL = new SpawnFigure(new int[,] {
-			{1,0},
-			{1,0},
-			{1,1}
-		});
+		figL = new SpawnFigure (6);
 
 		//Debug.Log(_figL.Rank.ToString());
 		//Debug.Log(_figL.GetUpperBound(0).ToString());
@@ -37,11 +38,12 @@ public class SpawnGrid : MonoBehaviour {
 				//Instantiate (grid_element);
 				GUI.Label(new Rect(ix, iy, 50, 50), "#");
 			} */
+		StartCoroutine(fallingFigures());
 	}
 
-	void CleArr() {
-		for (int iy=0; iy<20; iy++)
-			for (int ix=0; ix<10; ix++)
+	void CleArr(int[,] _clarr) {
+		for (int iy=0; iy<_clarr.GetUpperBound(1)+1; iy++)
+			for (int ix=0; ix<_clarr.GetUpperBound(0)+1; ix++)
 				_arr [ix, iy] = 0;
 	}
 
@@ -49,22 +51,45 @@ public class SpawnGrid : MonoBehaviour {
 		for (int iy=1; iy<21; iy++)
 		for (int ix=1; ix<11; ix++) {
 
-			if(_arr[ix-1, iy-1] == 0) GUI.Label(new Rect(ix*25, iy*25, 30, 30), texWhite);
-				else GUI.Label(new Rect(ix*25, iy*25, 30, 30), texBlue);
+			int cell = _arr[ix-1, iy-1];
+
+			if (cell == 0) GUI.Label(new Rect(ix*25, iy*25, 30, 30), texWhite);
+			else if (cell == 1) GUI.Label(new Rect(ix*25, iy*25, 30, 30), texBlue);
+			else if (cell == 2) GUI.Label(new Rect(ix*25, iy*25, 30, 30), texGreen);
 		}
 		//Debug.Log("draw");
 
 	}
-	
+
+	void CopyScreen(int[,] _source, int[,] _destination) {
+		for (int iy=0; iy<20; iy++)
+		for (int ix=0; ix<10; ix++) {
+			_destination[ix, iy] = _source[ix, iy];			
+		}
+
+	}
+
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.UpArrow))	figL.RotateFigure ();	//_y--;
-		if (Input.GetKeyDown (KeyCode.DownArrow))	figL.y++;
+		if (Input.GetKeyDown (KeyCode.DownArrow))	fallingSpeed = 0f;
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) figL.x--;
 		if (Input.GetKeyDown (KeyCode.RightArrow))	figL.x++;
-		CleArr ();
+
+
+		CleArr (_arr);
 		//_arr [_x, _y] = 1;
 
-		figL.DrawFigure(_arr);
+		CopyScreen (_fixed, _arr);
+
+		figL.DrawFigure(_arr, _fixed);
+
+		if (figL.flagCollision) {
+			Debug.Log("collision");
+			CopyScreen (_arr, _fixed);
+			_arr = new int[10,20];
+			fallingSpeed = 1f;
+			figL = new SpawnFigure (6);
+		}
 
 		/*
 		for (int iy=0; iy<=_figL.GetUpperBound(0); iy++)
@@ -72,5 +97,14 @@ public class SpawnGrid : MonoBehaviour {
 				if (_figL[iy, ix] == 1) _arr[ix+_x, iy+_y] = 1;
 			}
 			*/
+	}
+
+	IEnumerator fallingFigures()
+	{
+		while(true)
+		{
+			if(figL != null) figL.y++;
+			yield return new WaitForSeconds(fallingSpeed);
+		}
 	}
 }
